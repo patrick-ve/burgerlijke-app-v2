2c3ef6b3241e0588d2f49d7ea14bcffcac28c7d5
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-16 21:57:05 +0200

    chore: update seed contents

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -90,6 +90,7 @@ const recipes: SeedingRecipe[] = [
         },
       ],
     },
+    userId: '550e8400-e29b-41d4-a716-446655440000',
   },
   {
     name: 'Lasagna Bolognese',
@@ -143,6 +144,7 @@ const recipes: SeedingRecipe[] = [
         },
       ],
     },
+    userId: '550e8400-e29b-41d4-a716-446655440000',
   },
   {
     name: 'Fajitas',
@@ -218,6 +220,7 @@ const recipes: SeedingRecipe[] = [
         },
       ],
     },
+    userId: '550e8400-e29b-41d4-a716-446655440000',
   },
 ];
 
