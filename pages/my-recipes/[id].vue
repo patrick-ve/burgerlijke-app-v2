1c30e98dacea4de7402ef5237016504c81b7ccad
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-16 23:55:36 +0200

    wip: recipe detail page

--- pages/my-recipes/[id].vue
+++ pages/my-recipes/[id].vue
@@ -1,19 +1,35 @@
 <template>
-  <UContainer v-if="recipe">
-    <h1>{{ recipe.name }}</h1>
-    <p>{{ recipe.description }}</p>
-    <p>{{ recipe.cookingTime }}</p>
-    <p>{{ recipe }}</p>
-  </UContainer>
+  <section class="pt-4">
+    <UContainer v-if="recipe">
+      <h1 class="font-semibold text-xl mb-4">{{ recipe.name }}</h1>
+      <p>{{ recipe.description }}</p>
+      <p>{{ recipe.cookingTime }}</p>
+      <ul>
+        <li v-for="step in recipe.preparationSteps" :key="step.id">
+          {{ step.description }}
+        </li>
+      </ul>
+
+      <ul>
+        <li
+          v-for="ingredient in recipe.ingredients"
+          :key="ingredient.id"
+        >
+          {{ ingredient.name }} - {{ ingredient.amount }} -
+          {{ ingredient.unit }}
+        </li>
+      </ul>
+    </UContainer>
+  </section>
 </template>
 
 <script setup lang="ts">
-import type { Recipe } from '@prisma/client';
+import { CompleteRecipe } from '@/types';
 
 const route = useRoute();
 const { id } = route.params;
 
-const recipe = ref(null) as Ref<Recipe | null>;
+const recipe = ref(null) as Ref<CompleteRecipe | null>;
 
 onMounted(async () => {
   recipe.value = await $fetch(`/api/recipe/${id}`);
