af4a488130d36f0781966de2b20523ef022e431e
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-16 23:32:48 +0200

    dev: refactor store

--- stores/user.ts
+++ stores/user.ts
@@ -1,19 +1,24 @@
 import { defineStore } from 'pinia';
-
-const user = useSupabaseUser();
-const userId = user.value!.id;
+import type { Recipe } from '@prisma/client';
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    recipes: [],
+    recipes: [] as Recipe[],
   }),
 
   actions: {
     async fetchRecipes() {
+      const user = useSupabaseUser();
+      const userId = user.value!.id.toString();
+
       try {
-        this.recipes = await $fetch(`/api/recipe/${userId}`, {
-          method: 'GET',
-        });
+        const data = await $fetch(`/api/recipes/${userId}`);
+
+        this.recipes = data.map((recipe: any) => ({
+          ...recipe,
+          createdAt: new Date(recipe.createdAt),
+          updatedAt: new Date(recipe.updatedAt),
+        }));
       } catch (error) {
         console.error(error);
       }
