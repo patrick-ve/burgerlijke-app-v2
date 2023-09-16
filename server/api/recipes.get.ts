c1327a968a16fbf1e5a512f8e4f5d2c99a9a37d2
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-16 19:33:18 +0200

    feat: get recipes and relevant properties

--- server/api/recipes.get.ts
+++ server/api/recipes.get.ts
@@ -3,12 +3,25 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const recipe = await prisma.recipe.findUnique({
-    where: { id: 1 },
-    include: { preparationSteps: true, ingredients: true },
+  const recipe = await prisma.recipe.findMany({
+    include: {
+      preparationSteps: {
+        select: {
+          description: true,
+          isCompleted: true,
+        },
+      },
+      ingredients: {
+        select: {
+          name: true,
+          amount: true,
+          unit: true,
+        },
+      },
+    },
   });
 
-  console.log(recipe);
+  console.log(JSON.stringify(recipe, null, 2));
 
   if (!recipe) {
     throw createError({
