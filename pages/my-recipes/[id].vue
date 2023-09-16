dd953c0043d3310e97769cac032161a26311e983
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-16 23:32:15 +0200

    feat: dynamic recipe id page

---
+++ pages/my-recipes/[id].vue
@@ -0,0 +1,21 @@
+<template>
+  <UContainer v-if="recipe">
+    <h1>{{ recipe.name }}</h1>
+    <p>{{ recipe.description }}</p>
+    <p>{{ recipe.cookingTime }}</p>
+    <p>{{ recipe }}</p>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import type { Recipe } from '@prisma/client';
+
+const route = useRoute();
+const { id } = route.params;
+
+const recipe = ref(null) as Ref<Recipe | null>;
+
+onMounted(async () => {
+  recipe.value = await $fetch(`/api/recipe/${id}`);
+});
+</script>
--- pages/my-recipes/index.vue
+++ pages/my-recipes/index.vue
@@ -2,13 +2,23 @@
   <section class="pt-4">
     <UContainer>
       <h1 class="font-semibold text-xl mb-4">My recipes</h1>
-      <code>{{ store.recipes }}</code>
+
+      <nuxt-link
+        v-for="recipe in store.recipes"
+        :key="recipe.id"
+        :to="`/my-recipes/${recipe.id}`"
+      >
+        <article>
+          {{ recipe.name }} - {{ recipe.description }}
+        </article>
+      </nuxt-link>
     </UContainer>
   </section>
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
+
 const store = useUserStore();
 
 onMounted(() => {
