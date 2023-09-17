e1d45078c30fe584abe05ccc80977da543931116
Patrick van Everdingen <patrick@passionatepeople.io>
2023-09-17 12:12:45 +0200

    feat: amount transformer

---
+++ utils/amountTransformer.ts
@@ -0,0 +1,24 @@
+export function mapIngredientAmount(
+  unit: string,
+  amount: number | null
+): string {
+  const unitMap: { [key: string]: string } = {
+    GRAM: 'gram',
+    KILOGRAM: 'kilogram',
+    LITER: 'liter',
+    MILLILITER: 'milliliter',
+    PIECE: 'stuks',
+    TEASPOON: 'theelepels',
+    TABLESPOON: 'eetlepels',
+    CUP: 'kopjes',
+    PINCH: 'snufjes',
+    SOME: 'wat',
+    OTHER: 'anders',
+  };
+
+  if (amount === 1 || amount === 0 || amount === null) {
+    unitMap[unit].slice(0, -1);
+  }
+
+  return unitMap[unit] || unit;
+}
