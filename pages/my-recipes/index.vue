218697852ff3486957a398ce00e64160d46964be
Patrick van Everdingen <patrick@passionatepeople.io>
2023-10-18 15:37:43 +0200

    wip: create new recipe form

--- pages/my-recipes/index.vue
+++ pages/my-recipes/index.vue
@@ -3,6 +3,8 @@
     <UContainer>
       <h1 class="font-semibold text-xl mb-4">My recipes</h1>
 
+      <UIcon name="i-heroicons-arrow-down-right-20-solid" />
+
       <nuxt-link
         v-for="recipe in store.recipes"
         :key="recipe.id"
@@ -12,6 +14,24 @@
           {{ recipe.name }} - {{ recipe.description }}
         </article>
       </nuxt-link>
+
+      <UModal v-model="isModalOpen">
+        <UCard>
+          <template #header>
+            <h1 class="font-semibold text-xl">
+              Maak een nieuw recept aan
+            </h1>
+          </template>
+          <recipes-create-form />
+        </UCard>
+      </UModal>
+
+      <UButton
+        block
+        label="Maak een nieuw recept aan"
+        icon="i-heroicons-plus-circle"
+        @click="isModalOpen = true"
+      />
     </UContainer>
   </section>
 </template>
@@ -20,6 +40,8 @@
 import { useUserStore } from '@/stores/user';
 
 const store = useUserStore();
+
+const isModalOpen = ref(false);
 
 onMounted(() => {
   store.fetchRecipes();
