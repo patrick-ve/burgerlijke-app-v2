fef4cd4e5566f73bf1d07665e2045218fb1a3dfe
Patrick van Everdingen <patrick@passionatepeople.io>
2023-10-22 18:21:29 +0200

    wip: semantic search

---
+++ search.ts
@@ -0,0 +1,63 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseKey)
+  throw new Error(`Expected SUPABASE_SERVICE_ROLE_KEY`);
+
+const url = process.env.SUPABASE_URL;
+if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+
+export const run = async () => {
+  const client = createClient(url, supabaseKey);
+  console.log(url, supabaseKey);
+
+  const recipes = await client.from('recipes').select('*');
+  console.log(recipes);
+
+  const vectorStore = await SupabaseVectorStore.fromExistingIndex(
+    new OpenAIEmbeddings(),
+    {
+      client,
+      tableName: 'recipes',
+      queryName: 'match_recipes',
+    }
+  );
+
+  const resultOne = await vectorStore.similaritySearch(
+    'Vegetarian',
+    5
+  );
+
+  console.log(resultOne);
+
+  // Create Embedding
+
+  // const embeddings = new OpenAIEmbeddings();
+
+  // const generatedEmbedding = await embeddings.embedQuery(
+  //   'Vegetarian'
+  // );
+  // console.log(generatedEmbedding);
+
+  // // Search Supabase
+  // const { data, error } = await client.rpc('match_recipes', {
+  //   query_embedding: generatedEmbedding,
+  //   // similarity_threshold: 0.75,
+  //   // match_count: 3,
+  // });
+
+  // if (data) {
+  //   console.log(data);
+  // }
+
+  // if (error) {
+  //   console.log(error);
+  // }
+};
+
+run();
--- server/api/query/recipes.post.ts
+++ server/api/query/recipes.post.ts
@@ -1,12 +1,45 @@
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { serverSupabaseClient } from '#supabase/server';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+// import { serverSupabaseClient } from '#supabase/server';
+// import { PrismaClient } from '@prisma/client';
+import { createClient } from '@supabase/supabase-js';
+
+const client = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 
 export default defineEventHandler(async (event) => {
   const { query } = await readBody(event);
-  const client = await serverSupabaseClient(event);
 
-  const embedding = new OpenAIEmbeddings({
-    modelName: 'text-embedding-ada-002',
-    openAIApiKey: useRuntimeConfig().openAiKey,
-  });
+  // const recipes = await prisma.recipes.findMany();
+  // console.log(recipes);
+  const recipes = await client.from('recipes').select('*');
+  console.log(recipes);
+
+  // const embeddings = new OpenAIEmbeddings({
+  //   modelName: 'text-embedding-ada-002',
+  //   openAIApiKey: useRuntimeConfig().openAiKey,
+  // });
+
+  // const generatedEmbeddings = await embeddings.embedQuery(query);
+
+  // console.log(generatedEmbeddings);
+
+  // const { data: recipes, error } = await client.rpc(
+  //   'match_recipes',
+  //   // @ts-expect-error
+  //   {
+  //     query_embeddings: generatedEmbeddings,
+  //     similarity_threshold: 0.5,
+  //     match_count: 3,
+  //   }
+  // );
+
+  // console.log(recipes);
+
+  // if (error) console.error(error);
+  // else console.log(recipes);
 });
